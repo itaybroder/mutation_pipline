b385d0cc33849dcc744516db8d434223256c09f5
itaybroder <itay.broder@gmail.com>
2022-02-06 20:50:04 +0200

    added the vocs indicator to the df

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 from os import stat
 import pandas as pd
-from tools import PROTEIN, split_for_all_peptides
+from tools import PROTEIN, split_for_all_peptides, is_vocs
 
 #getting a list of all mutations from the file
-mutation_list = []
-with open("mutation.txt", "r") as f:
+voc_mutation_list = []
+mutation_list = pd.read_csv("all_mut.csv")["Mutation"]
+
+with open("voc_mutation.txt", "r") as f:
     lines = f.readlines()
 
 for line in lines:
-    mutation_list.append(line.replace(",", "").replace("'", "").replace("\n", "").replace(" ", ""))
+    voc_mutation_list.append(line.replace(",", "").replace("'", "").replace("\n", "").replace(" ", ""))
 #----------------------------------------------------------------------------------------------
 
 #getting all the peptides from the original protein
 peptides = split_for_all_peptides(PROTEIN)
 #---------------------------------------------------------------------------------------------
 
-cols = ["mutation poition", "mutation", "start_pos", "end_pos", "original_peptide", "after_mutation"]
+#creating the first mutation dataframe
+cols = ["mutation poition", "mutation", "start_pos", "end_pos", "original_peptide", "after_mutation", "Vocs"]
 rows = []
 
 for peptide in peptides:
     for mut in mutation_list:
+        if("-" in mut):
+            continue
         mut_pos = int(mut[1:-1])
         new_aa = mut[-1]
         start_pos = int(peptide[1])
@@ -30,8 +35,10 @@ for peptide in peptides:
             new_peptide = list(new_peptide)
             new_peptide[relative_index] = new_aa 
             new_peptide = "".join(new_peptide)
-            rows.append([mut_pos, new_aa, start_pos, end_pos, peptide[0], new_peptide])
+            rows.append([mut_pos, new_aa, start_pos, end_pos, peptide[0], new_peptide, is_vocs(mut, voc_mutation_list)])
     
 df = pd.DataFrame(rows, columns=cols)
 df.to_csv("mutation_peptide_df.csv")
-    
+#------------------------------------------------------------------------------------------------------------------
+
+
