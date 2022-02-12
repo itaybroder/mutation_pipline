8a70b16ec9e743a9aa36f9af11ebccfb85e92bfa
itaybroder <itay.broder@gmail.com>
2022-02-12 12:37:39 +0200

    added basic functions to the pipeline

--- predictors/config.py
+++ predictors/config.py
@@ -1,8 +1,8 @@
 import os
 
-HOME_DIRECTORY = '/home/itaybroder/Desktop/netchop_pipeline'
-INPUT_DIR = os.path.join(HOME_DIRECTORY, 'input_files')
-OUTPUT_DIR = os.path.join(HOME_DIRECTORY, 'output_files')
+HOME_DIRECTORY = '/home/itaybroder/Desktop/mutation_pipline'
+INPUT_DIR = os.path.join(HOME_DIRECTORY, 'data/input_files')
+OUTPUT_DIR = os.path.join(HOME_DIRECTORY, 'data/output_files')
 
 #tools
 PATH_TO_NETCHOP = '/home/itaybroder/Downloads/netchop-3.1'
